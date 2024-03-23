e7eeb31dc60dad7aef8f252468dc9181d932444c
komothecat <71205197+komothecat@users.noreply.github.com>
2024-03-23 19:39:01 +0800

    add rebar3

--- packages/rebar3/build.sh
+++ packages/rebar3/build.sh
@@ -6,9 +6,17 @@ TERMUX_PKG_HOMEPAGE=https://rebar3.org
 TERMUX_PKG_DESCRIPTION="A sophisticated build-tool for Erlang projects that follows OTP principles."
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=
-TERMUX_PKG_SRCURL=https://github.com/erlang/rebar3/archive/refs/tags/3.22.1.tar.gz
-TERMUX_PKG_SHA256=
+TERMUX_PKG_VERSION=3.22.1
+TERMUX_PKG_SRCURL=https://github.com/erlang/rebar3/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=2855b5784300865d2e43cb7a135cb2bba144cf15214c619065b918afc8cc6eb9
 TERMUX_PKG_DEPENDS="erlang"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
+
+termux_step_make() {
+	HOME=. escript bootstrap
+}
+
+termux_step_make_install() {
+	install -Dm755 -t "${TERMUX_PREFIX}/bin" rebar3
+}
