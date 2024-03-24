b7ec2520eb02cf73b619f257e33c191eab37b969
Komo <71205197+cattokomo@users.noreply.github.com>
2024-03-24 23:47:14 +0800

    update

--- packages/rebar3/build.sh
+++ packages/rebar3/build.sh
@@ -1,7 +1,3 @@
-# Skeleton build.sh script for new package.
-# For reference about available fields, check the Termux Developer's Wiki page:
-# https://github.com/termux/termux-packages/wiki/Creating-new-package
-
 TERMUX_PKG_HOMEPAGE=https://rebar3.org
 TERMUX_PKG_DESCRIPTION="A sophisticated build-tool for Erlang projects that follows OTP principles."
 TERMUX_PKG_LICENSE="Apache-2.0"
@@ -11,10 +7,9 @@ TERMUX_PKG_SRCURL=https://github.com/erlang/rebar3/archive/refs/tags/${TERMUX_PK
 TERMUX_PKG_SHA256=2855b5784300865d2e43cb7a135cb2bba144cf15214c619065b918afc8cc6eb9
 TERMUX_PKG_DEPENDS="erlang"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_PLATFORM_INDEPENDENT=true
+#TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
 termux_step_make() {
-	find /usr/lib/erlang/lib || 	find ${TERMUX_PREFIX}/lib/erlang/lib
 	escript bootstrap
 	install -Dm755 -t "${TERMUX_PREFIX}/bin" rebar3
 }
