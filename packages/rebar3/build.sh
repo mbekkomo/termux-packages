fc22dd178a68dd527be2105f79d2f42f53fb8ec6
komothecat <71205197+komothecat@users.noreply.github.com>
2024-06-09 14:28:48 +0800

    test

--- packages/rebar3/build.sh
+++ packages/rebar3/build.sh
@@ -2,17 +2,26 @@ TERMUX_PKG_HOMEPAGE=https://rebar3.org
 TERMUX_PKG_DESCRIPTION="A sophisticated build-tool for Erlang projects that follows OTP principles."
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=3.22.1
+TERMUX_PKG_VERSION=3.23.0
 TERMUX_PKG_SRCURL=https://github.com/erlang/rebar3/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=2855b5784300865d2e43cb7a135cb2bba144cf15214c619065b918afc8cc6eb9
+TERMUX_PKG_SHA256=00646b692762ffd340560e8f16486dbda840e1546749ee5a7f58feeb77e7b516
 TERMUX_PKG_DEPENDS="erlang"
 TERMUX_PKG_BUILD_DEPENDS="erlang"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
+termux_step_configure() {
+	mkdir ~/.erl
+	curl -O https://raw.githubusercontent.com/kerl/kerl/master/kerl
+	chmod +x kerl
+	./kerl build-install 27.0 27.0 ~/.erl
+	source ~/.erl/activate
+}
+
 termux_step_make() {
-	set -x
-	ERL_LIBS="$TERMUX_PREFIX/lib/erlang" "$TERMUX_PREFIX/bin/escript" bootstrap
-	install -Dm755 -t "${TERMUX_PREFIX}/bin" rebar3
-	set +x
+	escript bootstrap
+}
+
+termux_step_make_install() {
+	install -Dm700 rebar3 "${TERMUX_PREFIX}/bin/rebar3"
 }
